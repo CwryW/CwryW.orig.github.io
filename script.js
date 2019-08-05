2257a262d43cb3284dae948387026c3cf2a026c4
CwryW <35494300+CwryW@users.noreply.github.com>
2019-08-05 03:17:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
- 
-var myNav = document.getElementById("mynav");
-var btn = myNav.getElementsByClassName("btn");
-for (var i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function() {
-  var curr = document.getElementsByClassName("active");
-  curr[0].className = curr[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
- 
+  
